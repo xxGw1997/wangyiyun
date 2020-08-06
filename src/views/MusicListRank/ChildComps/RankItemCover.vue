184a1f8a425b7e7facb70799887de1ba2d8aaee6
xxgw <540040588@qq.com>
2020-08-06 23:01:05 +0800

    排行榜+榜单item

---
+++ src/views/MusicListRank/ChildComps/RankItemCover.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>榜单项目封面</div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  components: {}
+}
+</script>
+
+<style>
+</style>
--- src/views/MusicListRank/MusicListRank.vue
+++ src/views/MusicListRank/MusicListRank.vue
@@ -13,6 +13,117 @@
         </div>
       </header-top>
     </div>
+    <div class="rank-content">
+      <cube-scroll>
+        <div class="recommend-rank">榜单推荐</div>
+        <div class="official-rank">官方榜</div>
+        <div class="characteristic-rank">特色榜</div>
+        <div class="global-rank">全球榜</div>
+        <div class="region-rank">地区榜</div>
+        <div class="style-rank">曲风榜</div>
+        <div class="more-rank">更多榜单</div>
+        <li>1n</li>
+        <li>2n</li>
+        <li>3n</li>
+        <li>4n</li>
+        <li>5n</li>
+        <li>6n</li>
+        <li>7n</li>
+        <li>8n</li>
+        <li>9n</li>
+        <li>10n</li>
+        <li>11n</li>
+        <li>12n</li>
+        <li>13n</li>
+        <li>14n</li>
+        <li>15n</li>
+        <li>16n</li>
+        <li>17n</li>
+        <li>18n</li>
+        <li>19n</li>
+        <li>20n</li>
+        <li>21n</li>
+        <li>22n</li>
+        <li>23n</li>
+        <li>24n</li>
+        <li>25n</li>
+        <li>26n</li>
+        <li>27n</li>
+        <li>28n</li>
+        <li>29n</li>
+        <li>30n</li>
+        <li>31n</li>
+        <li>32n</li>
+        <li>33n</li>
+        <li>34n</li>
+        <li>35n</li>
+        <li>36n</li>
+        <li>37n</li>
+        <li>38n</li>
+        <li>39n</li>
+        <li>40n</li>
+        <li>41n</li>
+        <li>42n</li>
+        <li>43n</li>
+        <li>44n</li>
+        <li>45n</li>
+        <li>46n</li>
+        <li>47n</li>
+        <li>48n</li>
+        <li>49n</li>
+        <li>50n</li>
+        <li>51n</li>
+        <li>52n</li>
+        <li>53n</li>
+        <li>54n</li>
+        <li>55n</li>
+        <li>56n</li>
+        <li>57n</li>
+        <li>58n</li>
+        <li>59n</li>
+        <li>60n</li>
+        <li>61n</li>
+        <li>62n</li>
+        <li>63n</li>
+        <li>64n</li>
+        <li>65n</li>
+        <li>66n</li>
+        <li>67n</li>
+        <li>68n</li>
+        <li>69n</li>
+        <li>70n</li>
+        <li>71n</li>
+        <li>72n</li>
+        <li>73n</li>
+        <li>74n</li>
+        <li>75n</li>
+        <li>76n</li>
+        <li>77n</li>
+        <li>78n</li>
+        <li>79n</li>
+        <li>80n</li>
+        <li>81n</li>
+        <li>82n</li>
+        <li>83n</li>
+        <li>84n</li>
+        <li>85n</li>
+        <li>86n</li>
+        <li>87n</li>
+        <li>88n</li>
+        <li>89n</li>
+        <li>90n</li>
+        <li>91n</li>
+        <li>92n</li>
+        <li>93n</li>
+        <li>94n</li>
+        <li>95n</li>
+        <li>96n</li>
+        <li>97n</li>
+        <li>98n</li>
+        <li>99n</li>
+        <li>100n</li>
+      </cube-scroll>
+    </div>
   </div>
 </template>
 
@@ -55,6 +166,11 @@ export default {
       letter-spacing: 1px;
       color: rgba(0, 0, 0, 0.9);
     }
+  }
+  .rank-content {
+    height: calc(100vh - 110px);
+    margin: 0 10px;
+    background: pink;
   }
 }
 </style>
